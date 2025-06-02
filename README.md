202ba77631d8cc0b076284e931d12351d73432a3
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-06-02 20:55:50 -0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# cluster
-Repositório para criação de cluster
+Alunos
+- Kauan Henrique Bertalha RA:22262074-2 
+- Matheus Toscano Rossini RA: 22212262-2 
+- Willyan Santos Tomaz e Silva RA:22014128-2
---
+++ main.py
@@ -0,0 +1,29 @@
+from leitorcsv import CSVReader
+from centroide_inicial import initial_centroid
+from services.cluster import Cluster
+from calcular import Calculate
+
+def main():
+    csv_file = 'alunos1.csv'
+    students = CSVReader.readCsv(csv_file)
+    initialCentroids = initial_centroid(students)
+    initialClusters = [Cluster(initialCentroids[0]), Cluster(initialCentroids[1])]
+    calculate = Calculate(initialClusters)
+    
+    for student in students:
+        cluster = calculate.define_cluster_2(student)
+        
+        if (cluster):
+            cluster.addStudent(student)
+            
+    for cluster in calculate.clusters:
+        print(f"Cluster: {cluster}")
+        
+    pass
+
+    
+
+    
+    
+if __name__ == "__main__":
+    main()
